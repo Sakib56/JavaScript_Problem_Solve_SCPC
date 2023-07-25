ef9a8e98659d5ea1e791596fa3a9740b51ca6361
Sakib <sakib.hstu.bd@gmail.com>
2023-07-25 22:57:39 +0600

    task1 completet

---
+++ task1.js
@@ -0,0 +1,14 @@
+/* Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+Example Input: "hello world" Example Output: "dlrow olleh"*/
+
+
+function reverseString(inputString) {
+  let reversedStr = '';
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    reversedStr += inputString[i];
+  }
+  return reversedStr;
+}
+const input = "Programming Hero";
+const result = reverseString(input);
+console.log(result); 
